ecf27952218d4362338d06da797a3c593bef41f3
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-07-17 15:56:21 +0100

    updates to swagger on the enum endpoints and get all curricula

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/EnumTypeResource.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/EnumTypeResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,36 +20,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class EnumTypeResource {
 
-	@ApiOperation(value = "Lists all Assessment types",
-			notes = "Returns a list of all available Assessment Types",
-			response = ResponseEntity.class, responseContainer = "List")
+	@ApiOperation(value = "Lists all Assessment types that can be associated with Curricula",
+			notes = "Used by clients to retrieve all Assessment types that are currently available by this service. \n" +
+					"This allows clients to dynamically list out all options for particular fields so that we do not \n" +
+					"need to maintain a list on both the backend and client",
+			response = ResponseEntity.class, responseContainer = "List",
+			httpMethod = "GET", produces = "application/json", protocols = "http, https")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "All Assessment Types", response = ResponseEntity.class)})
-	@RequestMapping("/assessment-types")
+	@RequestMapping(value = "/assessment-types", method = RequestMethod.GET)
 	@PreAuthorize("hasAuthority('curriculum:view')")
 	public ResponseEntity<AssessmentType[]> getAllAssessmentTypes() {
 		return new ResponseEntity<>(AssessmentType.values(), HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "Lists all Curriculum Sub Types",
-			notes = "Returns a list of all available Curriculum Sub Types",
-			response = ResponseEntity.class, responseContainer = "List")
+	@ApiOperation(value = "Lists all Curriculum Sub Types that can be associated with Curricula",
+			notes = "Used by clients to retrieve all Curriculum Sub Types that are currently available by this service. \n" +
+					"This allows clients to dynamically list out all options for particular fields so that we do not \n" +
+					"need to maintain a list on both the backend and client",
+			response = ResponseEntity.class, responseContainer = "List",
+			httpMethod = "GET", produces = "application/json", protocols = "http, https")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "All Curriculum Sub Types", response = ResponseEntity.class)})
-	@RequestMapping("/curriculum-sub-types")
+	@RequestMapping(value = "/curriculum-sub-types", method = RequestMethod.GET)
 	@PreAuthorize("hasAuthority('curriculum:view')")
 	public ResponseEntity<CurriculumSubType[]> getAllCurriculumSubTypes() {
 		return new ResponseEntity<>(CurriculumSubType.values(), HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "Lists all Specialty Types",
-			notes = "Returns a list of all available Specialty Types",
-			response = ResponseEntity.class, responseContainer = "List")
+	@ApiOperation(value = "Lists all Specialty Types that can be associated with Specialty",
+			notes = "Used by clients to retrieve all Specialty Types that are currently available by this service. \n" +
+					"This allows clients to dynamically list out all options for particular fields so that we do not \n" +
+					"need to maintain a list on both the backend and client",
+			response = ResponseEntity.class, responseContainer = "List",
+			httpMethod = "GET", produces = "application/json", protocols = "http, https")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "All Specialty Types", response = ResponseEntity.class)})
-	@RequestMapping("/specialty-types")
+	@RequestMapping(value = "/specialty-types", method = RequestMethod.GET)
 	@PreAuthorize("hasAuthority('specialty:view')")
-	public ResponseEntity<SpecialtyType[]> getAllSpecialtyTypes(){
+	public ResponseEntity<SpecialtyType[]> getAllSpecialtyTypes() {
 		return new ResponseEntity<>(SpecialtyType.values(), HttpStatus.OK);
 	}
 
