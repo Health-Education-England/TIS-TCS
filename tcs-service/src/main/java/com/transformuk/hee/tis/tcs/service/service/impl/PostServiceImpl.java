8bc69bf4637aeefc64a6ad92140509983c0629f2
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-08-11 15:44:09 +0100

    sonar fixes

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/impl/PostServiceImpl.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/impl/PostServiceImpl.java
@@ -10,6 +10,7 @@ import com.transformuk.hee.tis.tcs.service.service.PostService;
 import com.transformuk.hee.tis.tcs.service.service.mapper.PostMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -24,23 +25,18 @@ import java.util.List;
 @Transactional
 public class PostServiceImpl implements PostService {
 
-  private final Logger log = LoggerFactory.getLogger(PostServiceImpl.class);
+  private static final Logger log = LoggerFactory.getLogger(PostServiceImpl.class);
 
-  private final PostRepository postRepository;
-  private final PostGradeRepository postGradeRepository;
-  private final PostSiteRepository postSiteRepository;
-  private final PostSpecialtyRepository postSpecialtyRepository;
-  private final PostMapper postMapper;
-
-  public PostServiceImpl(PostRepository postRepository, PostGradeRepository postGradeRepository,
-                         PostSiteRepository postSiteRepository, PostSpecialtyRepository postSpecialtyRepository,
-                         PostMapper postMapper) {
-    this.postRepository = postRepository;
-    this.postGradeRepository = postGradeRepository;
-    this.postSiteRepository = postSiteRepository;
-    this.postSpecialtyRepository = postSpecialtyRepository;
-    this.postMapper = postMapper;
-  }
+  @Autowired
+  private PostRepository postRepository;
+  @Autowired
+  private PostGradeRepository postGradeRepository;
+  @Autowired
+  private PostSiteRepository postSiteRepository;
+  @Autowired
+  private PostSpecialtyRepository postSpecialtyRepository;
+  @Autowired
+  private PostMapper postMapper;
 
   /**
    * Save a post.
