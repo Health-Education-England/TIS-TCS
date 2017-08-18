06cef80900eaa00440fe8e1eac16754caebef939
srochani <sunil.rochani@hotmail.com>
2017-08-18 11:25:05 +0100

    Post Entity N+1 issue fix

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/repository/PostRepository.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.transformuk.hee.tis.tcs.service.repository;
 import com.transformuk.hee.tis.tcs.service.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -13,5 +15,14 @@ import java.util.Set;
  */
 @SuppressWarnings("unused")
 public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+
+  @EntityGraph(value = "all.oldNewPost.programmes.sites.grades.specialties.placementHistory", type= EntityGraph.EntityGraphType.FETCH)
+  @Override
+  Page<Post> findAll(Pageable pageable);
+
+  @EntityGraph(value = "all.oldNewPost.programmes.sites.grades.specialties.placementHistory", type= EntityGraph.EntityGraphType.FETCH)
+  @Override
+  Page<Post> findAll(Specification<Post> specification, Pageable pageable);
+
   Page<Post> findByManagingLocalOfficeIn(Set<String> deaneries, Pageable pageable);
 }
