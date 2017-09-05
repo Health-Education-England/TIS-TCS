6f717e7328af1e8c7e6777d82c88238d2e848538
srochani <sunil.rochani@hotmail.com>
2017-09-05 11:55:24 +0100

    Custom Mapper for TrainingNumber

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/TrainingNumberMapper.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/TrainingNumberMapper.java
@@ -1,31 +1,106 @@
 package com.transformuk.hee.tis.tcs.service.service.mapper;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.transformuk.hee.tis.tcs.api.dto.*;
 import com.transformuk.hee.tis.tcs.service.model.*;
+import org.apache.commons.collections4.CollectionUtils;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Mapper for the entity TrainingNumber and its DTO TrainingNumberDTO.
  */
-@Mapper(componentModel = "spring", uses = {ProgrammeMapper.class,CurriculumMapper.class})
-public interface TrainingNumberMapper {
+@Component
+public class TrainingNumberMapper {
 
-  TrainingNumberDTO trainingNumberToTrainingNumberDTO(TrainingNumber trainingNumber);
-
-  List<TrainingNumber> trainingNumberDTOsToTrainingNumbers(List<TrainingNumberDTO> trainingNumberDTOs);
-
-  List<TrainingNumberDTO> trainingNumbersToTrainingNumberDTOs(List<TrainingNumber> trainingNumbers);
-
-  TrainingNumber trainingNumberDTOToTrainingNumber(TrainingNumberDTO trainingNumberDTO);
-
-  default Programme programmeFromId(Long id){
-    if(id == null){
-      return null;
+  public TrainingNumberDTO trainingNumberToTrainingNumberDTO(TrainingNumber trainingNumber){
+    TrainingNumberDTO result = null;
+    if(trainingNumber != null){
+      result = toDTO(trainingNumber);
     }
-    Programme programme = new Programme();
-    programme.setId(id);
-    return programme;
+    return result;
   }
+
+  public TrainingNumber trainingNumberDTOToTrainingNumber(TrainingNumberDTO trainingNumberDTO) {
+    TrainingNumber result = toEntity(trainingNumberDTO);
+    return result;
+  }
+
+  public List<TrainingNumber> trainingNumberDTOsToTrainingNumbers(List<TrainingNumberDTO> trainingNumberDTOs) {
+    List<TrainingNumber> result =  Lists.newArrayList();
+
+    for (TrainingNumberDTO trainingNumberDTO : trainingNumberDTOs) {
+      result.add(toEntity(trainingNumberDTO));
+    }
+
+    return result;
+  }
+
+  public List<TrainingNumberDTO> trainingNumbersToTrainingNumberDTOs(List<TrainingNumber> trainingNumbers) {
+    List<TrainingNumberDTO> result =  Lists.newArrayList();
+
+    for (TrainingNumber trainingNumber : trainingNumbers) {
+      result.add(toDTO(trainingNumber));
+    }
+
+    return result;
+  }
+
+  private TrainingNumberDTO toDTO(TrainingNumber trainingNumber) {
+    TrainingNumberDTO result = new TrainingNumberDTO();
+
+    result.setId(trainingNumber.getId());
+    result.setNumber(trainingNumber.getNumber());
+    result.setAppointmentYear(trainingNumber.getAppointmentYear());
+    result.setSuffix(trainingNumber.getSuffix());
+    result.setTrainingNumberType(trainingNumber.getTrainingNumberType());
+    result.setTypeOfContract(trainingNumber.getTypeOfContract());
+    result.setProgramme(programmeToProgrammeDTO(trainingNumber.getProgramme()));
+    return result;
+  }
+
+  private ProgrammeDTO programmeToProgrammeDTO(Programme programme) {
+    ProgrammeDTO result = null;
+    if (programme != null) {
+      result = new ProgrammeDTO();
+      result.setId(programme.getId());
+      result.setIntrepidId(programme.getIntrepidId());
+      result.setProgrammeNumber(programme.getProgrammeNumber());
+      result.setProgrammeName(programme.getProgrammeName());
+      result.setManagingDeanery(programme.getManagingDeanery());
+      result.setStatus(programme.getStatus());
+    }
+    return result;
+  }
+  private Programme programmeDTOToProgramme(ProgrammeDTO programmeDTO) {
+    Programme result = null;
+    if (programmeDTO != null) {
+      result = new Programme();
+
+      result.setId(programmeDTO.getId());
+      result.setIntrepidId(programmeDTO.getIntrepidId());
+      result.setProgrammeNumber(programmeDTO.getProgrammeNumber());
+      result.setProgrammeName(programmeDTO.getProgrammeName());
+      result.setManagingDeanery(programmeDTO.getManagingDeanery());
+      result.setStatus(programmeDTO.getStatus());
+    }
+    return result;
+  }
+
+  private TrainingNumber toEntity(TrainingNumberDTO trainingNumberDTO) {
+    TrainingNumber result = new TrainingNumber();
+    result.setId(trainingNumberDTO.getId());
+    result.setNumber(trainingNumberDTO.getNumber());
+    result.setAppointmentYear(trainingNumberDTO.getAppointmentYear());
+    result.setSuffix(trainingNumberDTO.getSuffix());
+    result.setTrainingNumberType(trainingNumberDTO.getTrainingNumberType());
+    result.setTypeOfContract(trainingNumberDTO.getTypeOfContract());
+    result.setProgramme(programmeDTOToProgramme(trainingNumberDTO.getProgramme()));
+    return result;
+  }
+
 }
