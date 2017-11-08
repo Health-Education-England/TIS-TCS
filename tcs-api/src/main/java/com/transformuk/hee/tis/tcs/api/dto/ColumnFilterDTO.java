b23a71b1e6da1fd37f896c8664b6c579e6602201
Tamer Moustafa <tamer.moustafa@transformuk.com>
2017-11-08 14:17:41 +0000

    Add endpoint for filtering Posts

---
+++ tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/dto/ColumnFilterDTO.java
@@ -0,0 +1,45 @@
+package com.transformuk.hee.tis.tcs.api.dto;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public class ColumnFilterDTO {
+
+  @NotNull
+  private String name;
+
+  @NotNull
+  private List<String> values;
+
+  public ColumnFilterDTO() {
+  }
+
+  public ColumnFilterDTO(String name, List<String> values) {
+    this.name = name;
+    this.values = values;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<String> getValues() {
+    return values;
+  }
+
+  public void setValues(List<String> values) {
+    this.values = values;
+  }
+
+  @Override
+  public String toString() {
+    return "ColumnFilterDTO{" +
+        "name='" + name + '\'' +
+        ", values=" + values +
+        '}';
+  }
+}
