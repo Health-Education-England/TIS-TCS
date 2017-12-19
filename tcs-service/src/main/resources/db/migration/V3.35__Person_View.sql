a8400f7c608cc932ef0f9caceb42db4060908188
alexdobre <alx.dobre@gmail.com>
2017-12-19 11:41:29 +0000

    TISDEV-3392 Make the person list use the person view - UI and Server - rebased on master and made the necessary changes

--- tcs-service/src/main/resources/db/migration/V3.35__Person_View.sql
+++ tcs-service/src/main/resources/db/migration/V3.35__Person_View.sql
@@ -1,12 +1,3 @@
-CREATE TABLE `PersonOwner` (
-  `id` bigint(20) NOT NULL,
-  `owner` varchar(255) DEFAULT NULL,
-  `rule` varchar(255) DEFAULT NULL,
-  UNIQUE KEY `person_owner_pk` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `PersonOwner` ADD CONSTRAINT `fk_person_owner_person_id` FOREIGN KEY (`id`) REFERENCES `Person` (`id`);
-
 create or replace view `PersonView` as
   select p.id,
     p.intrepidId,
--- tcs-service/src/test/resources/db/migration/V3.35__Person_View.sql
+++ tcs-service/src/test/resources/db/migration/V3.35__Person_View.sql
@@ -1,13 +1,3 @@
-CREATE TABLE `PersonOwner` (
-  `id` bigint(20) NOT NULL,
-  `owner` varchar(255) DEFAULT NULL,
-  `rule` varchar(255) DEFAULT NULL,
-  UNIQUE KEY `person_owner_pk` (`id`)
-);
-
-ALTER TABLE `PersonOwner` ADD CONSTRAINT `fk_person_owner_person_id` FOREIGN KEY (`id`) REFERENCES `Person` (`id`);
-
-
 -- Because our testing runs on an H2 in memory database and H2 does not like our PersonView syntax, we overwrite
 -- this database migration step with a table instead of a view
 CREATE TABLE `PersonView` (
