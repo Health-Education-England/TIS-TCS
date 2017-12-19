1da743d2a6745ecd2f4cf2a52d813447fd361ed2
alexdobre <alx.dobre@gmail.com>
2017-12-19 13:48:08 +0000

    TISDEV-3392 Make the person list use the person view - UI and Server

--- tcs-service/src/main/resources/db/migration/V3.35__Person_View.sql
+++ tcs-service/src/main/resources/db/migration/V3.35__Person_View.sql
@@ -1,45 +1,47 @@
-create or replace view `PersonView` as
-  select p.id,
-    p.intrepidId,
-    cd.surname,
-    cd.forenames,
-    gmc.gmcNumber,
-    gdc.gdcNumber,
-    p.publicHealthNumber,
-    pm.programmeId,
-    pm.programmeName,
-    pm.programmeNumber,
-    pm.TrainingNumber,
-    pl.gradeAbbreviation,
-    pl.siteCode,
-    pl.placementType,
-    p.role,
-    p.status,
-    COALESCE(pm.lo, pl.lo) as currentOwner,
-    CASE WHEN (pm.lo IS NOT NULL) THEN 'P1' ELSE 'P2' END as currentOwnerRule
-  from Person p
-    left join ContactDetails cd on (cd.id = p.id)
-    left join GmcDetails gmc on (gmc.id = p.id)
-    left join GdcDetails gdc on (gdc.id = p.id)
-    left join (select pm.personid,
-                 pm.programmeStartDate,
-                 pm.programmeEndDate,
-                 pm.programmeId,
-                 prg.programmeName,
-                 prg.programmeNumber,
-                 tn.number as TrainingNumber,
-                 prg.managingDeanery as lo
-               from ProgrammeMembership pm
-                 join Programme prg on (prg.id = pm.programmeId)
-                 join TrainingNumber tn on (tn.programmeId = pm.programmeId)) pm on (pm.personId = p.id
-                                                                                     and curdate() between pm.programmeStartDate and pm.programmeEndDate)
-    left join (select pl.traineeId,
-                 pl.dateFrom,
-                 pl.dateTo,
-                 pl.gradeAbbreviation,
-                 pl.siteCode,
-                 pl.placementType,
-                 pst.managingLocalOffice as lo
-               from Placement pl
-                 join Post pst on (pst.id = pl.postId)) pl on (pl.traineeId = p.id
-                                                               and curdate() between pl.dateFrom and pl.dateTo)
+create or replace view `PersonView`
+as
+select p.id,
+       p.intrepidId,
+       cd.surname,
+       cd.forenames,
+       gmc.gmcNumber,
+       gdc.gdcNumber,
+       p.publicHealthNumber,
+       pm.programmeId,
+       pm.programmeName,
+       pm.programmeNumber,
+       pm.TrainingNumber as trainingNumber,
+       pl.gradeAbbreviation,
+       pl.siteCode,
+       pl.placementType,
+       p.role,
+       p.status,
+       lo.owner as currentOwner,
+       lo.rule as currentOwnerRule
+from Person p
+left join ContactDetails cd on (cd.id = p.id)
+left join GmcDetails gmc on (gmc.id = p.id)
+left join GdcDetails gdc on (gdc.id = p.id)
+left join (select pm.personid,
+          pm.programmeStartDate,
+          pm.programmeEndDate,
+                  pm.programmeId,
+          prg.programmeName,
+          prg.programmeNumber,
+                  tn.number as TrainingNumber,
+          prg.managingDeanery as lo
+       from ProgrammeMembership pm
+           join Programme prg on (prg.id = pm.programmeId)      
+       join TrainingNumber tn on (tn.programmeId = pm.programmeId)) pm on (pm.personId = p.id
+                                                           and curdate() between pm.programmeStartDate and pm.programmeEndDate)
+left join (select pl.traineeId,
+          pl.dateFrom,
+          pl.dateTo,
+          pl.gradeAbbreviation,
+          pl.siteCode,
+          pl.placementType,
+          pst.managingLocalOffice as lo
+       from Placement pl
+       join Post pst on (pst.id = pl.postId)) pl on (pl.traineeId = p.id
+                           and curdate() between pl.dateFrom and pl.dateTo)
+left join PersonOwner lo on (lo.id = p.id);
