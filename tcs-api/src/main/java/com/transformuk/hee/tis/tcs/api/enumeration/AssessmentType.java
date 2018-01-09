5cf3664224ba1a4e4c01b5f85951f09c19fa8780
paulhoang-hee <paul.hoang@hee.nhs.uk>
2018-01-09 11:28:36 +0000

    deprecate the assessment type enum endpoint (#97)

--- tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/enumeration/AssessmentType.java
+++ tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/enumeration/AssessmentType.java
@@ -2,7 +2,10 @@ package com.transformuk.hee.tis.tcs.api.enumeration;
 
 /**
  * The AssessmentType enumeration.
+ *
+ * Deprecated: Please see the AssessmentType entity in the Reference service
  */
+@Deprecated
 public enum AssessmentType {
   ARCP, RITA, ACADEMIC
 }
