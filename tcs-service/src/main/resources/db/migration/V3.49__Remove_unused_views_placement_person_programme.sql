01624025a2f09de51ad0116b249153e93f7a2125
simonmeredith <simonmeredith1@gmail.com>
2018-01-16 11:29:19 +0000

    Remove views (#99)
    * added flyway script to remove unused views
    
    * abumped pom

---
+++ tcs-service/src/main/resources/db/migration/V3.49__Remove_unused_views_placement_person_programme.sql
@@ -0,0 +1,6 @@
+DROP VIEW IF EXISTS `personview`;
+DROP VIEW IF EXISTS `personview_serial`;
+DROP VIEW IF EXISTS `placements`;
+DROP VIEW IF EXISTS `placements_desc`;
+DROP VIEW IF EXISTS `programmes`;
+DROP VIEW IF EXISTS `programmes_desc`;
