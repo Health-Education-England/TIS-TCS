997c9383323c3772b06fe6e9431b1b56d62d1c8d
srochani <sunil.rochani@hotmail.com>
2018-02-02 13:37:37 +0000

    Qualification enum type updates

--- tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/enumeration/QualificationType.java
+++ tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/enumeration/QualificationType.java
@@ -1,49 +1,34 @@
 package com.transformuk.hee.tis.tcs.api.enumeration;
 
 public enum QualificationType {
-  HIGHER_DEGREE("Higher Degree"),
-  BASIC_DEGREE("Basic Degree"),
-  BACHELORS_DEGREE_HONS("Bachelors Degree Hons"),
-  BACHELORS_DEGREE("Bachelors Degree"),
-  DIPLOMA("Diploma"),
-  NON_MEDICAL_DEGREE("Non Medical Degree"),
-  BM("BM"),
-  PRIMARY_QUALIFICATION("Primary qualificatio"),
-  MB_BS("MB BS"),
-  BM_BS("BM BS"),
-  MB_CHB("MB ChB"),
-  MB_BCHIR("MB BChir"),
-  MB_BCH("MB BCh"),
-  BM_BCH("BM BCh"),
-  NVQ("NVQ"),
-  CERTIFICATION("Certification"),
-  COLLEGE_FACULTY("College/Faculty"),
-  BDS("BDS"),
-  MD("MD"),
-  PHD("PhD"),
-  STATE_EXAM("State Exam"),
-  HIGHER_QUALIFICATION("Higher qualification"),
-  LEKARZ("Lekarz"),
-  MASTERS_DEGREE("Masters Degree"),
-  MBBS_OR_BSC("MB BS/BSc"),
-  MEDICINE("Medicine"),
-  BMBS("BMBS"),
-  MBBS("MBBS"),
-  MBCHB("MbChB"),
-  MEDICINE_MBCHB("Medicine MBChB");
-
-  private String name;
-
-  QualificationType(String name) {
-    this.name = name;
-  }
-
-  public String getName(){
-    return this.name;
-  }
-
-  @Override
-  public String toString() {
-    return name;
-  }
+  HIGHER_DEGREE,
+  BASIC_DEGREE,
+  BACHELORS_DEGREE_HONS,
+  BACHELORS_DEGREE,
+  DIPLOMA,
+  NON_MEDICAL_DEGREE,
+  BM,
+  PRIMARY_QUALIFICATION,
+  MB_BS,
+  BM_BS,
+  MB_CHB,
+  MB_BCHIR,
+  MB_BCH,
+  BM_BCH,
+  NVQ,
+  CERTIFICATION,
+  COLLEGE_FACULTY,
+  BDS,
+  MD,
+  PHD,
+  STATE_EXAM,
+  HIGHER_QUALIFICATION,
+  LEKARZ,
+  MASTERS_DEGREE,
+  MBBS_OR_BSC,
+  MEDICINE,
+  BMBS,
+  MBBS,
+  MBCHB,
+  MEDICINE_MBCHB
 }
