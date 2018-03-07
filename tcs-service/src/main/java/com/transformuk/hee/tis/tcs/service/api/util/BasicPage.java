061d1e6c7f5f9ee11574cddce1a7668c947d708e
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-03-07 11:54:00 +0000

    TISDEV-4061 split search endpoints to not use count query and only get one more record to see if there is a next page

---
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/util/BasicPage.java
@@ -0,0 +1,56 @@
+package com.transformuk.hee.tis.tcs.service.api.util;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BasicPage<T> {
+
+  private List<T> content;
+  private Pageable pageable;
+  private boolean hasNext;
+
+  public BasicPage(List<T> content, Pageable pageable, boolean hasNext) {
+    this.content = content;
+    this.pageable = pageable;
+    this.hasNext = hasNext;
+  }
+
+  public BasicPage(List<T> content, Pageable pageable) {
+    this.content = content;
+    this.pageable = pageable;
+    this.hasNext = false;
+  }
+
+  public int getNumber() {
+    return pageable == null ? 0 : pageable.getPageNumber();
+  }
+
+  public int getSize() {
+    return pageable == null ? 0 : pageable.getPageSize();
+  }
+
+  public List<T> getContent() {
+    return content;
+  }
+
+  public void setContent(List<T> content) {
+    this.content = content;
+  }
+
+  public Pageable getPageable() {
+    return pageable;
+  }
+
+  public void setPageable(Pageable pageable) {
+    this.pageable = pageable;
+  }
+
+  public boolean isHasNext() {
+    return hasNext;
+  }
+
+  public void setHasNext(boolean hasNext) {
+    this.hasNext = hasNext;
+  }
+}
