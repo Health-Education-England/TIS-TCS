de4ec0d349307126e9ca4d927a9b124bb853b223
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-04-23 11:17:39 +0100

    new endpoint to search for posts by national post number TISDEV-4265

---
+++ tcs-service/src/main/resources/queries/searchPostView.sql
@@ -0,0 +1,21 @@
+select distinct ot.*
+from (
+  SELECT p.`id`,
+    pg.`gradeId` as `approvedGradeId`,
+    ps.`specialtyId` as `primarySpecialtyId`,
+    sp.`specialtyCode` as `primarySpecialtyCode`,
+    sp.`name` as `primarySpecialtyName`,
+    pst.`siteId` as `primarySiteId`,
+    p.`nationalPostNumber`,
+    p.`status`,
+    p.`owner`
+    FROM `Post` p
+    LEFT JOIN `PostGrade` pg on p.`id` = pg.`postId` AND pg.`postGradeType` = 'APPROVED'
+    LEFT JOIN `PostSpecialty` ps on p.`id` = ps.`postId` AND ps.`postSpecialtyType` = 'PRIMARY'
+    LEFT JOIN `Specialty` sp on sp.`id` = ps.`specialtyId`
+    LEFT JOIN `PostSite` pst on p.`id` = pst.`postId` AND pst.`postSiteType` = 'PRIMARY'
+ WHERECLAUSE
+ ORDERBYCLAUSE
+ LIMITCLAUSE
+) as ot
+;
