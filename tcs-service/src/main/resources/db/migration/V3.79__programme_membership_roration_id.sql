b52327775cfa8fa7715fe4131f6f9852fc160e76
Luis Matos <luis.matos@hee.nhs.uk>
2018-06-27 12:21:24 +0100

    add database constraints to prevent duplicates

--- tcs-service/src/main/resources/db/migration/V3.79__programme_membership_roration_id.sql
+++ tcs-service/src/main/resources/db/migration/V3.79__programme_membership_roration_id.sql
@@ -3,3 +3,9 @@ ADD COLUMN `rotationId` bigint NULL;
 
 ALTER TABLE `ProgrammeMembership`
 ADD CONSTRAINT `fk_programme_membership_rotation_id` FOREIGN KEY (`rotationId`) REFERENCES `Rotation` (`id`);
+
+ALTER TABLE `RotationPost`
+ADD CONSTRAINT `uk_post_id_rotation_id` UNIQUE (postId, rotationId);
+
+ALTER TABLE `Rotation`
+ADD CONSTRAINT `uk_programme_id_name` UNIQUE (programmeId, name);
