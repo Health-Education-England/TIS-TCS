0e49c77a60b53a3fb05800ecf3ddac8b47af62ed
simonmeredith <simonmeredith1@gmail.com>
2018-09-24 14:26:45 +0100

    add fundingTypeId to post funding and update the DTO and model class

--- tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/enumeration/FundingType.java
+++ tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/enumeration/FundingType.java
@@ -4,5 +4,7 @@ package com.transformuk.hee.tis.tcs.api.enumeration;
  * The FundingType enumeration.
  */
 public enum FundingType {
-  TARIFF, MADEL, TRUST, OTHER
+  TARIFF, MADEL, TRUST, OTHER, HEE_FUNDED_TARIFF,
+  HEE_FUNDED_NON_TARIFF, TRUST_FUNDED, ACADEMIC_NIHR,
+  ACADEMIC_HEE, ACADEMIC_TRUST, SUPERNUMERARY
 }
