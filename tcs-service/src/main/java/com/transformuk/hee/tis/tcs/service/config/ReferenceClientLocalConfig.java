b93ea9a45ee673d046ecc44987cc1336b3850ce4
paulhoang-hee <paul.hoang@hee.nhs.uk>
2018-11-12 11:20:12 +0000

    fix to java config that only runs in certain spring profiles

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/ReferenceClientLocalConfig.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/ReferenceClientLocalConfig.java
@@ -1,13 +1,12 @@
 package com.transformuk.hee.tis.tcs.service.config;
 
-import com.transformuk.hee.tis.reference.client.impl.ReferenceServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Profile("dev")
+@Profile("local")
 public class ReferenceClientLocalConfig extends com.transformuk.hee.tis.reference.client.config.ReferenceClientConfig {
 
   @Bean
--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/ReferenceClientProdConfig.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/ReferenceClientProdConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Profile("prod")
+@Profile({"dev", "stage", "prod", "uidev"})
 public class ReferenceClientProdConfig extends com.transformuk.hee.tis.reference.client.config.ReferenceClientConfig {
 
   @Bean
