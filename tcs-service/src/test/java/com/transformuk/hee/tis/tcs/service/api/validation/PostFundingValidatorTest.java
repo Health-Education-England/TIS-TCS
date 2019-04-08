0f269f941a0fecb6d76df152aafbed6ef4e97a66
Tuomas Alahäivälä <tuomas.alahaivala@leadin.fi>
2019-04-08 11:24:51 +0100

    update unit tests for post funding validator

--- tcs-service/src/test/java/com/transformuk/hee/tis/tcs/service/api/validation/PostFundingValidatorTest.java
+++ tcs-service/src/test/java/com/transformuk/hee/tis/tcs/service/api/validation/PostFundingValidatorTest.java
@@ -9,7 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.time.LocalDate;
 import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PostFundingValidatorTest {
@@ -19,6 +23,10 @@ public class PostFundingValidatorTest {
   String NOT_FOUND_ERROR = "funding type does not exist.";
   String MULTIPLE_FOUND_ERROR = "found multiple funding type.";
   private Set<PostFundingDTO> pfDTOs;
+  private Map<PostFundingDTO, List<String>> checkedMap;
+  private PostFundingDTO idMissingDTO;
+  private PostFundingDTO notOtherDTO;
+  private PostFundingDTO validDTO;
 
   @Mock
   private ReferenceServiceImpl referenceService;
@@ -26,42 +34,64 @@ public class PostFundingValidatorTest {
   @InjectMocks
   private PostFundingValidator postFundingValidator;
 
-  @Before
-  public void setup() {
+  private Map<PostFundingDTO, List<String>> buildCheckedMap(PostFundingDTO pfDTO) {
     pfDTOs = new HashSet<>();
-    PostFundingDTO validDTO = new PostFundingDTO();
-    pfDTOs.add(validDTO);
+    pfDTOs.add(pfDTO);
 
     // prepare a map
     Iterator iter = pfDTOs.iterator();
-    Map<PostFundingDTO, List<String>> checkedMap = new HashMap<>();
+    checkedMap = new HashMap<>();
     while (iter.hasNext()) {
       checkedMap.put((PostFundingDTO)iter.next(), new ArrayList<>());
     }
+    return checkedMap;
   }
 
-  @Test
-  public void testValidatesCorrectData() {
-    postFundingValidator.validateFundingType(checkedMap);
+  @Before
+  public void setup() {
+    validDTO = new PostFundingDTO();
+    validDTO.setId(1L);
+    validDTO.setFundingType("Other");
+    validDTO.setInfo("info");
+    validDTO.setFundingBodyId("foo");
+    validDTO.setStartDate(LocalDate.now());
+    validDTO.setStartDate(LocalDate.now().plusMonths(1));
+
+    idMissingDTO = new PostFundingDTO();
+    validDTO.setFundingType("Other");
+    validDTO.setInfo("info");
+    validDTO.setFundingBodyId("foo");
+    validDTO.setStartDate(LocalDate.now());
+    validDTO.setStartDate(LocalDate.now().plusMonths(1));
+
+
+    notOtherDTO = new PostFundingDTO();
+    validDTO.setFundingType("type1234");
+    validDTO.setInfo("info");
+    validDTO.setFundingBodyId("foo");
+    validDTO.setStartDate(LocalDate.now());
+    validDTO.setStartDate(LocalDate.now().plusMonths(1));
   }
 
   @Test
   public void testValidateFailsIfIdIsEmpty() {
+    checkedMap = buildCheckedMap(idMissingDTO);
 
+    Map<PostFundingDTO, List<String>> result = postFundingValidator.validateFundingType(checkedMap);
+    assertThat(result.get(idMissingDTO).contains(FUNDING_TYPE_EMPTY), is(true));
   }
 
   @Test
-  public void testValidateFailsIfNoInfoGivenForOtherFundingType() {
+  public void testValidateFailsInfoGivenForFundingTypeNoOther() {
+    checkedMap = buildCheckedMap(notOtherDTO);
+    Map<PostFundingDTO, List<String>> result = postFundingValidator.validateFundingType(checkedMap);
+    assertThat(result.get(idMissingDTO).contains(FUNDING_TYPE_NOT_OTHER_ERROR), is(true));
 
   }
 
   @Test
   public void testValidateFailsIfFundingTypeIsNotUnique() {
-
+    // Map<PostFundingDTO, List<String>> result = postFundingValidator.validateFundingType(checkedMap);
   }
-
-
-
-
 
 }
