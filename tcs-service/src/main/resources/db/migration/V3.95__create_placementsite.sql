a6d0f3db131aa68edddb17fee3871759e16bb322
jayanta2018 <jayanta.saha@hee.nhs.uk>
2019-04-12 12:32:45 +0100

    Added the flyway script to add PlacementSite table to TCS

---
+++ tcs-service/src/main/resources/db/migration/V3.95__create_placementsite.sql
@@ -0,0 +1,11 @@
+CREATE TABLE PlacementSite (
+   id bigint(20) NOT NULL AUTO_INCREMENT,
+   placementId bigint(20) NOT NULL,
+   siteId bigint(20) NULL DEFAULT NULL,
+   placementSiteType varchar(255) NULL DEFAULT NULL,
+   UNIQUE KEY placement_site_pk (id),
+   CONSTRAINT fk_placement_site FOREIGN KEY (placementId) REFERENCES Placement (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
