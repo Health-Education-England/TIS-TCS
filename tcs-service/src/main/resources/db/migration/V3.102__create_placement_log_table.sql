ccbd43698d1684b47abf73d3395e9523fce9d054
Yafang Deng <hihilary@outlook.com>
2019-12-04 16:19:28 +0000

    Fix datetime issue for PlacementLog

--- tcs-service/src/main/resources/db/migration/V3.102__create_placement_log_table.sql
+++ tcs-service/src/main/resources/db/migration/V3.102__create_placement_log_table.sql
@@ -19,8 +19,8 @@ CREATE TABLE `PlacementLog` (
   `lifecycleState` enum('DRAFT','APPROVED') DEFAULT NULL,
   `placementId` bigint(20) NOT NULL,
   `logType` enum('CREATE','UPDATE','DELETE') DEFAULT NULL,
-  `validDateFrom` date DEFAULT NULL,
-  `validDateTo` date DEFAULT NULL,
+  `validDateFrom` datetime DEFAULT NULL,
+  `validDateTo` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_placement_approval_record_placement_id` FOREIGN KEY (`placementId`) REFERENCES `Placement` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
