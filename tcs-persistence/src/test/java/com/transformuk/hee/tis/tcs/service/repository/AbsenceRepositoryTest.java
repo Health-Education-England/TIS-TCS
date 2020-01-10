fb4bc26761b0d2a7d7300e92365802d6598ca000
Paul Hoang <paul.hoang@hee.nhs.uk>
2020-01-10 17:47:56 +0000

    add absence repo test

--- tcs-persistence/src/test/java/com/transformuk/hee/tis/tcs/service/repository/AbsenceRepositoryTest.java
+++ tcs-persistence/src/test/java/com/transformuk/hee/tis/tcs/service/repository/AbsenceRepositoryTest.java
@@ -1,15 +1,9 @@
 package com.transformuk.hee.tis.tcs.service.repository;
 
-import static org.junit.Assert.*;
-
-import com.transformuk.hee.tis.tcs.api.enumeration.Status;
 import com.transformuk.hee.tis.tcs.service.TestConfig;
 import com.transformuk.hee.tis.tcs.service.model.Absence;
-import com.transformuk.hee.tis.tcs.service.model.Post;
-import java.util.List;
+import java.time.LocalDate;
 import java.util.Optional;
-import javax.persistence.EntityManager;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,7 +23,8 @@ public class AbsenceRepositoryTest {
 
   @Test
   @Sql(scripts = {"/scripts/absence.sql"})
-  @Sql(scripts = {"/scripts/deleteAbsence.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+  @Sql(scripts = {
+      "/scripts/deleteAbsence.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
   public void findPostsForProgrammeIdShouldReturnLinkedPostThatAreActiveOnly() {
 
     Optional<Absence> result = absenceRepository.findByAbsenceAttendanceId("11122");
@@ -37,10 +32,16 @@ public class AbsenceRepositoryTest {
     Assert.assertTrue(result.isPresent());
     Absence absence = result.get();
     Assert.assertEquals(absence.getId(), new Long(2));
-    Assert.assertEquals(absence.getPerson(), new Long(1));
+    Assert.assertEquals(absence.getPerson().getId(), new Long(1));
     Assert.assertEquals(absence.getDurationInDays(), new Long(30L));
-//    Assert.assertEquals(absence.getId());
-//    Assert.assertEquals(absence.getId());
+    LocalDate startDate = absence.getStartDate();
+    Assert.assertEquals(startDate.getYear(), 2020);
+    Assert.assertEquals(startDate.getMonthValue(), 1);
+    Assert.assertEquals(startDate.getDayOfMonth(), 1);
+    LocalDate endDate = absence.getEndDate();
+    Assert.assertEquals(endDate.getYear(), 2021);
+    Assert.assertEquals(endDate.getMonthValue(), 2);
+    Assert.assertEquals(endDate.getDayOfMonth(), 3);
   }
 
 }
