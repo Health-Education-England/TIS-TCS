dd835d784ac2d7c995a54e00b960bfd055db6bf4
Andy Dingley <judge40@gmail.com>
2020-03-10 14:08:46 +0000

    fix: avoid cyclic reference from mapper
    The QualificationMapper and PersonMapper cause a cyclic mapping, remove
    the PersonMapper usage from QualificationMapper to avoid this. Add
    PersonDTO to/from Person mappings to the QualificationMapper as a
    workaround.
    
    TISNEW-3686

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/QualificationMapper.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/QualificationMapper.java
@@ -1,14 +1,17 @@
 package com.transformuk.hee.tis.tcs.service.service.mapper;
 
+import com.transformuk.hee.tis.tcs.api.dto.PersonDTO;
 import com.transformuk.hee.tis.tcs.api.dto.QualificationDTO;
+import com.transformuk.hee.tis.tcs.service.model.Person;
 import com.transformuk.hee.tis.tcs.service.model.Qualification;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity Qualification and its DTO QualificationDTO.
  */
-@Mapper(componentModel = "spring", uses = {PersonMapper.class})
+@Mapper(componentModel = "spring")
 public interface QualificationMapper {
 
   QualificationDTO toDto(Qualification qualification);
@@ -18,4 +21,14 @@ public interface QualificationMapper {
   List<Qualification> toEntities(List<QualificationDTO> qualificationDtos);
 
   List<QualificationDTO> toDTOs(List<Qualification> qualifications);
+
+  // TODO: Look at other options to avoid cyclic mapping such as using @Context.
+  @Mapping(target = "programmeMemberships", ignore = true)
+  @Mapping(target = "qualifications", ignore = true)
+  PersonDTO personToPersonDto(Person person);
+
+  // TODO: Look at other options to avoid cyclic mapping such as using @Context.
+  @Mapping(target = "programmeMemberships", ignore = true)
+  @Mapping(target = "qualifications", ignore = true)
+  Person personDtoToPerson(PersonDTO personDto);
 }
