5096a1660547507ed85cf3150c5fe8feddf67b96
Andy Dingley <judge40@gmail.com>
2020-05-21 16:31:40 +0100

    feat: Add validation for TrainerApprovalDTO
    TISNEW-3857

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/validation/TrainerApprovalValidator.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/validation/TrainerApprovalValidator.java
@@ -1,8 +1,9 @@
 package com.transformuk.hee.tis.tcs.service.api.validation;
 
-import com.transformuk.hee.tis.reference.client.impl.ReferenceServiceImpl;
 import com.transformuk.hee.tis.tcs.api.dto.TrainerApprovalDTO;
+import com.transformuk.hee.tis.tcs.api.enumeration.ApprovalStatus;
 import com.transformuk.hee.tis.tcs.service.repository.PersonRepository;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,48 +14,90 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Component
 public class TrainerApprovalValidator {
+
   private static final String TRAINER_APPROVAL_DTO_NAME = "TrainerApprovalDTO";
 
-  private PersonRepository personRepository;
-  private ReferenceServiceImpl referenceService;
+  private final PersonRepository personRepository;
 
   @Autowired
-  public TrainerApprovalValidator(PersonRepository personRepository,
-    ReferenceServiceImpl referenceService) {
+  public TrainerApprovalValidator(PersonRepository personRepository) {
     this.personRepository = personRepository;
-    this.referenceService = referenceService;
   }
 
-  public void validate(TrainerApprovalDTO trainerApprovalDTO) throws MethodArgumentNotValidException {
-
+  public void validate(TrainerApprovalDTO dto) throws MethodArgumentNotValidException {
     List<FieldError> fieldErrors = new ArrayList<>();
-    fieldErrors.addAll(checkPerson(trainerApprovalDTO));
+    fieldErrors.addAll(checkPerson(dto));
+    fieldErrors.addAll(checkStartDate(dto));
+    fieldErrors.addAll(checkEndDate(dto));
+    fieldErrors.addAll(checkApprovalStatus(dto));
+
     if (!fieldErrors.isEmpty()) {
-      BeanPropertyBindingResult bindingResult = new BeanPropertyBindingResult(trainerApprovalDTO,
-        TRAINER_APPROVAL_DTO_NAME);
+      BeanPropertyBindingResult bindingResult =
+          new BeanPropertyBindingResult(dto, TRAINER_APPROVAL_DTO_NAME);
       fieldErrors.forEach(bindingResult::addError);
       throw new MethodArgumentNotValidException(null, bindingResult);
     }
   }
 
-  private List<FieldError> checkPerson(TrainerApprovalDTO trainerApprovalDTO) {
+  private List<FieldError> checkPerson(TrainerApprovalDTO dto) {
     List<FieldError> fieldErrors = new ArrayList<>();
     // check the Person
-    if (trainerApprovalDTO.getPerson() == null || trainerApprovalDTO.getPerson().getId() == null) {
+    if (dto.getPerson() == null || dto.getPerson().getId() == null) {
       requireFieldErrors(fieldErrors, "person");
-    } else if (trainerApprovalDTO.getPerson() != null
-      && trainerApprovalDTO.getPerson().getId() != null) {
-      if (!personRepository.existsById(trainerApprovalDTO.getPerson().getId())) {
+    } else if (dto.getPerson() != null && dto.getPerson().getId() != null) {
+      if (!personRepository.existsById(dto.getPerson().getId())) {
         fieldErrors.add(new FieldError(TRAINER_APPROVAL_DTO_NAME, "person",
-          String
-            .format("Person with id %d does not exist", trainerApprovalDTO.getPerson().getId())));
+            String.format("Person with id %d does not exist", dto.getPerson().getId())));
       }
     }
     return fieldErrors;
   }
 
+  private List<FieldError> checkStartDate(TrainerApprovalDTO dto) {
+    List<FieldError> fieldErrors = new ArrayList<>();
+
+    LocalDate startDate = dto.getStartDate();
+    LocalDate endDate = dto.getEndDate();
+
+    if (startDate != null && endDate != null && startDate.isAfter(endDate)) {
+      FieldError fieldError = new FieldError(TRAINER_APPROVAL_DTO_NAME, "startDate",
+          "startDate must be before endDate.");
+      fieldErrors.add(fieldError);
+    }
+
+    return fieldErrors;
+  }
+
+  private List<FieldError> checkEndDate(TrainerApprovalDTO dto) {
+    List<FieldError> fieldErrors = new ArrayList<>();
+
+    if (dto.getStartDate() != null && dto.getEndDate() == null) {
+      FieldError fieldError = new FieldError(TRAINER_APPROVAL_DTO_NAME, "endDate",
+          "endDate is required when startDate is populated.");
+      fieldErrors.add(fieldError);
+    }
+
+    return fieldErrors;
+  }
+
+  private List<FieldError> checkApprovalStatus(TrainerApprovalDTO dto) {
+    List<FieldError> fieldErrors = new ArrayList<>();
+
+    LocalDate startDate = dto.getStartDate();
+    LocalDate endDate = dto.getEndDate();
+    ApprovalStatus approvalStatus = dto.getApprovalStatus();
+
+    if ((startDate != null || endDate != null) && approvalStatus == null) {
+      FieldError fieldError = new FieldError(TRAINER_APPROVAL_DTO_NAME, "approvalStatus",
+          "approvalStatus is required when dates are populated.");
+      fieldErrors.add(fieldError);
+    }
+
+    return fieldErrors;
+  }
+
   private void requireFieldErrors(List<FieldError> fieldErrors, String field) {
     fieldErrors.add(new FieldError(TRAINER_APPROVAL_DTO_NAME, field,
-      String.format("%s is required", field)));
+        String.format("%s is required", field)));
   }
 }
