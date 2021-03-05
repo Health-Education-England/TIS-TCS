c2ba52ff91a60170adf73fe78b234358bbf3c6d8
Doris Wong <doris.wong@hee.nhs.uk>
2021-03-05 07:02:35 +0000

    fix: not to send rabbit message if rabbit doesn't exist in the env (nimdta) to avoid error logs
    TIS21-1168

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/RabbitConfig.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/RabbitConfig.java
@@ -1,6 +1,8 @@
 package com.transformuk.hee.tis.tcs.service.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -11,11 +13,15 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty("spring.rabbitmq.host")
 @Configuration
 public class RabbitConfig {
+
+  private final Logger log = LoggerFactory.getLogger(RabbitConfig.class);
 
   @Value("${app.rabbit.queue}")
   private String queueName;
@@ -55,6 +61,7 @@ public class RabbitConfig {
     final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
     rabbitTemplate.setMessageConverter(jsonMessageConverter());
     rabbitTemplate.containerAckMode(AcknowledgeMode.AUTO);
+    log.info("creating rabbit template {} ", rabbitTemplate);
     return rabbitTemplate;
   }
 }
