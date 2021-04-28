fafb2aedf779c865d360d024a4889d40c5b41df3
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-04-28 13:38:04 +0100

    code smells: remove unused imports

--- tcs-service/src/test/java/com/transformuk/hee/tis/tcs/service/service/impl/PlacementServiceImplTest.java
+++ tcs-service/src/test/java/com/transformuk/hee/tis/tcs/service/service/impl/PlacementServiceImplTest.java
@@ -7,7 +7,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
