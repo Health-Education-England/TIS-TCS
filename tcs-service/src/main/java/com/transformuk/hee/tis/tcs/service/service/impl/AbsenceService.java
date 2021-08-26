06918571368183c0fbb614a6ad569ce341176ddc
Andy Dingley <Judge40@users.noreply.github.com>
2021-08-26 17:37:55 +0100

    refactor: Remove reflection from AbsenceService
    The AbsenceService uses reflection when patching an object to find the
    fields given in a hashmap.
    Remove the reflection and instead use a `patch` method in the mapper
    that updates each field of the target Absence if the field exists in the
    parameter map.
    
    TIS21-1900

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/impl/AbsenceService.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/impl/AbsenceService.java
@@ -1,18 +1,13 @@
 package com.transformuk.hee.tis.tcs.service.service.impl;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
 import com.transformuk.hee.tis.tcs.api.dto.AbsenceDTO;
 import com.transformuk.hee.tis.tcs.service.model.Absence;
 import com.transformuk.hee.tis.tcs.service.model.Person;
 import com.transformuk.hee.tis.tcs.service.repository.AbsenceRepository;
 import com.transformuk.hee.tis.tcs.service.repository.PersonRepository;
 import com.transformuk.hee.tis.tcs.service.service.mapper.AbsenceMapper;
-import java.lang.reflect.Field;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import org.apache.commons.lang3.StringUtils;
@@ -26,11 +21,6 @@ public class AbsenceService {
 
   private static final String ABSENCE_ID_KEY = "id";
   private static final String ABSENCE_ATTENDANCE_ID_KEY = "absenceAttendanceId";
-  private static final String PERSON_ID_KEY = "personId";
-  private static final String DURATION_IN_DAYS_KEY = "durationInDays";
-  private static final String START_DATE_KEY = "startDate";
-  private static final String END_DATE_KEY = "endDate";
-  public static final String AMENDED_DATE_KEY = "amendedDate";
 
   @Autowired
   private AbsenceMapper absenceMapper;
@@ -93,18 +83,16 @@ public class AbsenceService {
     return result.orElseThrow(() -> new RuntimeException("Failed to create new Absence record"));
   }
 
-
-  public Optional<AbsenceDTO> patchAbsence(Map<String, Object> absenceDtoPatchMap)
-      throws Exception {
+  public Optional<AbsenceDTO> patchAbsence(Map<String, Object> absenceDtoPatchMap) {
     Preconditions.checkArgument(absenceDtoPatchMap != null, "absenceDto cannot be null");
 
-    Map<String, Object> params = fixPatchParams(absenceDtoPatchMap);
     Optional<Absence> optionalAbsence = Optional.empty();
     if (absenceDtoPatchMap.containsKey(ABSENCE_ID_KEY)) {
-      optionalAbsence = absenceRepository.findById((Long) params.get(ABSENCE_ID_KEY));
+      long id = ((Integer) absenceDtoPatchMap.get(ABSENCE_ID_KEY)).longValue();
+      optionalAbsence = absenceRepository.findById(id);
     } else if (absenceDtoPatchMap.containsKey(ABSENCE_ATTENDANCE_ID_KEY)) {
       optionalAbsence = absenceRepository
-          .findByAbsenceAttendanceId((String) params.get(ABSENCE_ATTENDANCE_ID_KEY));
+          .findByAbsenceAttendanceId((String) absenceDtoPatchMap.get(ABSENCE_ATTENDANCE_ID_KEY));
     }
 
     if (!optionalAbsence.isPresent()) {
@@ -114,47 +102,8 @@ public class AbsenceService {
     entityManager.detach(foundAbsence); //must detach as changes to the entity while attached wont
     //trigger a version check, even if we change the version
     foundAbsence.setAmendedDate(null);
-    for (Entry<String, Object> entry : params.entrySet()) {
-      String fieldName = entry.getKey();
-      Object fieldValue = entry.getValue();
 
-      if (StringUtils.equals(PERSON_ID_KEY, fieldName) ||
-          StringUtils.equals(ABSENCE_ID_KEY, fieldName)) {
-        continue;
-      }
-
-      Field absenceField = Absence.class.getDeclaredField(fieldName);
-      absenceField.setAccessible(true);
-      absenceField.set(foundAbsence, fieldValue);
-    }
-
+    absenceMapper.patch(absenceDtoPatchMap, foundAbsence);
     return Optional.of(absenceMapper.toDto(absenceRepository.saveAndFlush(foundAbsence)));
-  }
-
-  //because the endpoint converts json to a maps, it only knows about simple types and not the types
-  //of the absenceDTO, so this is to fix the types so that they will match the Absence entity
-  private Map<String, Object> fixPatchParams(Map<String, Object> params) {
-    Map<String, Object> result = Maps.newHashMap();
-
-    for (Entry<String, Object> entry : params.entrySet()) {
-      String key = entry.getKey();
-      if (entry.getValue() == null) {
-        result.put(key, entry.getValue());
-      } else {
-        if (StringUtils.equals(key, ABSENCE_ID_KEY) || StringUtils
-            .equals(key, DURATION_IN_DAYS_KEY)) {
-          result.put(key, new Long((Integer) entry.getValue()));
-        } else if (StringUtils.equals(key, START_DATE_KEY) ||
-            StringUtils.equals(key, END_DATE_KEY)) {
-          result.put(key, LocalDate.parse((String) entry.getValue()));
-        } else if (StringUtils.equals(key, AMENDED_DATE_KEY)) {
-          result.put(key, LocalDateTime.parse((String) entry.getValue()));
-        } else {
-          result.put(key, entry.getValue());
-        }
-      }
-    }
-
-    return result;
   }
 }
--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/AbsenceMapper.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/AbsenceMapper.java
@@ -2,8 +2,12 @@ package com.transformuk.hee.tis.tcs.service.service.mapper;
 
 import com.transformuk.hee.tis.tcs.api.dto.AbsenceDTO;
 import com.transformuk.hee.tis.tcs.service.model.Absence;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
@@ -16,4 +20,30 @@ public interface AbsenceMapper extends EntityMapper<AbsenceDTO, Absence> {
   @Override
   @Mappings(@Mapping(source = "person.id", target = "personId"))
   AbsenceDTO toDto(Absence entity);
+
+  default void patch(Map<String, Object> source, @MappingTarget Absence target) {
+
+    target.setAbsenceAttendanceId(
+        (String) source.getOrDefault("absenceAttendanceId", target.getAbsenceAttendanceId()));
+
+    if (source.containsKey("durationInDays")) {
+      int durationInDays = (int) source.get("durationInDays");
+      target.setDurationInDays((long) durationInDays);
+    }
+
+    if (source.containsKey("startDate")) {
+      String startDate = (String) source.get("startDate");
+      target.setStartDate(LocalDate.parse(startDate));
+    }
+
+    if (source.containsKey("endDate")) {
+      String endDate = (String) source.get("endDate");
+      target.setEndDate(LocalDate.parse(endDate));
+    }
+
+    if (source.containsKey("amendedDate")) {
+      String amendedDate = (String) source.get("amendedDate");
+      target.setAmendedDate(LocalDateTime.parse(amendedDate));
+    }
+  }
 }
