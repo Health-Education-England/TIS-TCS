8b05bc84964e0f6c903050303c2ca1b0bb0f7daa
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-09-01 15:19:41 +0100

    chore: tidy-up to make a single SQL file.

--- tcs-service/src/main/resources/db/migration/data/nimdta/V4.7__UpdatePersonStatusWithLapsedPmsTIS21_2020.sql
+++ tcs-service/src/main/resources/db/migration/data/nimdta/V4.7__UpdatePersonStatusWithLapsedPmsTIS21_2020.sql
@@ -3,3 +3,9 @@ SET `status` = 'INACTIVE'
 WHERE
 p.status <> 'INACTIVE' AND
 p.id NOT IN (SELECT `personId` FROM `ProgrammeMembership` pm WHERE `programmeStartDate` <= curdate() AND `programmeEndDate` >= curdate() GROUP BY `personId`);
+
+UPDATE `Person` p
+SET `status` = 'CURRENT'
+WHERE p.status <> 'CURRENT' AND
+p.id IN (SELECT `personId` FROM `ProgrammeMembership` pm WHERE `programmeStartDate` <= curdate() AND `programmeEndDate` >= curdate() GROUP BY `personId`);
+
