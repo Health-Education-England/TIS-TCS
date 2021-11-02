d8f4898188f6317feb9062fb049bd7ab68b7e97f
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-11-02 16:53:27 +0000

    feat: Make flyway script handle csv field
    Roles are concatenated and delimited by a comma in the 'Role' column.
    This set of commands fixes all roles including the ones that have
    more than one value.
    Without this, only the roles that had one value could be fixed.
    
    TIS21-1629

--- tcs-service/src/main/resources/db/migration/data/hee/V4.10__fix_person_roles_with_inaccurate_casing.sql
+++ tcs-service/src/main/resources/db/migration/data/hee/V4.10__fix_person_roles_with_inaccurate_casing.sql
@@ -1,3 +1,37 @@
-update Person person
-inner join reference.Role role ON person.role = role.code
-set person.role = role.code;
+create temporary table Numbers as
+  select 1 as n
+  union select 2 as n
+  union select 3 as n
+  union select 4 as n
+  union select 5 as n
+  union select 6 as n
+  union select 7 as n
+  union select 8 as n
+  union select 9 as n
+  union select 10 as n;
+
+-- Create table where each row has a Person id and one role
+create temporary table PersonPerRole as
+select id,
+substring_index(substring_index(role, ',', n), ',', -1) as role
+from Person
+join Numbers on char_length(role) - char_length(replace(role, ',', '')) >= n - 1;
+
+-- Update each row's role with the role taken from the reference Role table (with correct casing)
+update PersonPerRole
+inner join reference.Role ref_role on PersonPerRole.role = ref_role.code
+set PersonPerRole.role = ref_role.code;
+
+-- Merge rows with same id back into one, where role is concatenated
+create temporary table ConcatenatedRoles as
+select id, group_concat(role separator ',') as role
+from PersonPerRole
+group by id;
+
+update Person
+inner join ConcatenatedRoles on Person.id = ConcatenatedRoles.id
+set Person.role = ConcatenatedRoles.role;
+
+drop table Numbers;
+drop table PersonPerRole;
+drop table ConcatenatedRoles;
