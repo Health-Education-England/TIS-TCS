c579caa8878a45a401696d54271b02781a75ba94
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-12-29 13:05:10 +0000

    fix: ensure correct CurriculumMembership table
    Auto_increment and index creation must mirror existing
    ProgrammeMembership table.

--- tcs-service/src/main/resources/db/migration/schema/V4.12__copy_ProgrammeMembership_to_CurriculumMembership.sql
+++ tcs-service/src/main/resources/db/migration/schema/V4.12__copy_ProgrammeMembership_to_CurriculumMembership.sql
@@ -1,1 +1,7 @@
-CREATE TABLE CurriculumMembership AS SELECT * FROM ProgrammeMembership;
+CREATE TABLE CurriculumMembership LIKE ProgrammeMembership;
+INSERT INTO CurriculumMembership SELECT * FROM ProgrammeMembership;
+SET @correct_auto_increment = (SELECT AUTO_INCREMENT FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = 'tcs' AND TABLE_NAME = 'ProgrammeMembership');
+SET @sql = CONCAT('ALTER TABLE `CurriculumMembership` AUTO_INCREMENT = ', @correct_auto_increment);
+PREPARE st FROM @sql;
+EXECUTE st;
