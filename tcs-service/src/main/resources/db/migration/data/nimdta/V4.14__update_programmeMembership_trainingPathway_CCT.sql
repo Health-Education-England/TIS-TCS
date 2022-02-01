347bb8370df7d2126c1b5925f2920c5a771368f3
Yafang Deng <hihilary@outlook.com>
2022-02-01 09:36:06 +0000

    chore: fix trainingPathway data of ProgrammeMembership
    TIS21-2381

---
+++ tcs-service/src/main/resources/db/migration/data/nimdta/V4.14__update_programmeMembership_trainingPathway_CCT.sql
@@ -0,0 +1,2 @@
+-- -- trainingPathway should be set to CCT, 25 in total
+UPDATE `ProgrammeMembership` SET `trainingPathway` = 'CCT' where `id` in (1695, 885, 901, 1696, 892, 913, 1491, 903, 148, 639, 2873, 6517, 141, 1261, 3714, 1691, 1257, 1256, 1251, 1253, 1259, 1258, 1255, 1254, 1252);
