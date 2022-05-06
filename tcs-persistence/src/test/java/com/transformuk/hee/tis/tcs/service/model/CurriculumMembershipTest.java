bc4f720a0b0f17391cb820da7e44816220547fcc
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2022-05-06 16:47:39 +0100

    test tweak

--- tcs-persistence/src/test/java/com/transformuk/hee/tis/tcs/service/model/CurriculumMembershipTest.java
+++ tcs-persistence/src/test/java/com/transformuk/hee/tis/tcs/service/model/CurriculumMembershipTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class CurriculumMembershipTest extends CurriculumMembership {
+public class CurriculumMembershipTest {
   private static final String LEAVING_REASON = "leaving reason";
   private static final String LEAVING_DESTINATION = "leaving destination";
   private static final String ROTATION_NAME = "rotation";
   private static final LocalDate START_DATE = LocalDate.of(2020, 1, 1);
   private static final LocalDate END_DATE = LocalDate.of(2020, 2, 1);
 
-  private CurriculumMembership curriculumMembership;
+  private final CurriculumMembership curriculumMembership;
 
   public CurriculumMembershipTest() {
     Rotation rotation = new Rotation();
