168ace051756e8b9b9c86ec643db77dcb7e387d7
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2023-05-17 17:25:18 +0100

    fix: remove unneeded dto serialize override

--- tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/dto/ConditionsOfJoiningDto.java
+++ tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/dto/ConditionsOfJoiningDto.java
@@ -3,7 +3,6 @@ package com.transformuk.hee.tis.tcs.api.dto;
 import com.transformuk.hee.tis.tcs.api.enumeration.GoldGuideVersion;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
 import java.util.UUID;
 import lombok.Data;
 
@@ -16,23 +15,4 @@ public class ConditionsOfJoiningDto implements Serializable {
   private UUID programmeMembershipUuid;
   private Instant signedAt;
   private GoldGuideVersion version;
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    ConditionsOfJoiningDto that = (ConditionsOfJoiningDto) o;
-    return Objects.equals(programmeMembershipUuid, that.getProgrammeMembershipUuid())
-        && Objects.equals(signedAt, that.getSignedAt())
-        && Objects.equals(version, that.getVersion());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(programmeMembershipUuid, signedAt, version);
-  }
 }
