09ea493231d4555f2186b13a2a55a351fb686513
Cai Willis <Cai.Willis@hee.nhs.uk>
2023-07-11 11:29:17 +0100

    chore: code smell
    TIS21-4548

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/RevalidationResource.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/api/RevalidationResource.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import javax.validation.constraints.NotBlank;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
