cb737fee557bb2164e94f13205eb49e2da1578ea
Yafang Deng <hihilary@outlook.com>
2023-09-06 16:54:02 +0100

    fix:code smells
    TIS21-3785

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/ProgrammeMembershipDtoMapper.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/service/mapper/ProgrammeMembershipDtoMapper.java
@@ -12,15 +12,25 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 /**
  * Mapper for copying fields from a programme membership source dto to the target dto.
+ * This mapper is used to copy fields from bulk upload ProgrammeMembershipDto to the dto to save.
  */
-@Mapper(componentModel = "spring") //, uses = {RotationDtoMapper.class})
+@Mapper(componentModel = "spring")
 public interface ProgrammeMembershipDtoMapper {
 
   @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
       nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
-  @Mapping(target = "rotation", expression = "java(mapRotationDto(source.getRotation(), target.getRotation()))")
+  @Mapping(target = "rotation", expression = "java("
+      + "mapRotationDto(source.getRotation(), target.getRotation()))")
   void copyIfNotNull(ProgrammeMembershipDTO source, @MappingTarget ProgrammeMembershipDTO target);
 
+  /**
+   * Map rotation name from source to target.
+   * Bulk upload only sends rotation name, so when rotation name is specified, create a new dto.
+   *
+   * @param sourceDto source Rotation Dto
+   * @param targetDto target Rotation Dto
+   * @return the targetDto to set the field
+   */
   default RotationDTO mapRotationDto(RotationDTO sourceDto, RotationDTO targetDto) {
     if (sourceDto == null || StringUtils.isEmpty(sourceDto.getName())) {
       return targetDto;
