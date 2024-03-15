ebbf1218d0367e67cdf3c0e67f43dc2b70524300
Cai.Willis <cai.willis@hee.nhs.uk>
2024-03-15 12:45:24 +0000

    fix: temporarily disable safe mode for fundingStatus intialisation
    TIS21-5808: Add a new field fundingStatus in Post

--- tcs-service/src/main/resources/db/migration/schema/v4.30__add_fundingstatus_field_to_post_and_populate.sql
+++ tcs-service/src/main/resources/db/migration/schema/v4.30__add_fundingstatus_field_to_post_and_populate.sql
@@ -1,10 +1,12 @@
 ALTER TABLE `Post` ADD COLUMN `fundingStatus` varchar(255) NOT NULL;
 
+SET SQL_SAFE_UPDATES = 0;
 UPDATE Post p
 LEFT JOIN (SELECT f.postId, MAX(f.endDate) AS endDate FROM PostFunding f GROUP BY f.postId) latestpf ON p.id = latestpf.postId
 SET p.fundingStatus = 'CURRENT'
 WHERE latestpf.endDate >= current_date()
 OR latestpf.endDate IS NULL;
+SET SQL_SAFE_UPDATES = 1;
 
 UPDATE Post p
 LEFT JOIN (SELECT f.postId, MAX(f.endDate) AS endDate FROM PostFunding f GROUP BY f.postId) latestpf ON p.id = latestpf.postId
