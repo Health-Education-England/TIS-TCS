dfd45c63c11c2bc3f74c5c0b017fd9c83a603bda
Cai.Willis <cai.willis@hee.nhs.uk>
2024-03-18 11:38:26 +0000

    fix: correctly update null post with funding end date as null - again
    TIS21-5808: Add a new field fundingStatus in Post

--- tcs-service/src/main/resources/db/migration/schema/v4.30__add_fundingstatus_field_to_post_and_populate.sql
+++ tcs-service/src/main/resources/db/migration/schema/v4.30__add_fundingstatus_field_to_post_and_populate.sql
@@ -2,14 +2,14 @@ ALTER TABLE `Post` ADD COLUMN `fundingStatus` varchar(255) NOT NULL;
 
 UPDATE Post p
 LEFT JOIN (SELECT f.postId, MAX(f.endDate) AS endDate FROM PostFunding f GROUP BY f.postId) latestpf ON p.id = latestpf.postId
-SET p.fundingStatus = 'CURRENT'
-WHERE latestpf.endDate >= current_date()
-
-UPDATE Post p
-INNER JOIN (SELECT f.postId, f.endDate AS endDate FROM PostFunding f WHERE endDate IS NULL) latestpf ON p.id = latestpf.postId
-SET p.fundingStatus = 'CURRENT';
+SET p.fundingStatus = 'INACTIVE'
+WHERE latestpf.endDate < current_date();
 
 UPDATE Post p
 LEFT JOIN (SELECT f.postId, MAX(f.endDate) AS endDate FROM PostFunding f GROUP BY f.postId) latestpf ON p.id = latestpf.postId
-SET p.fundingStatus = 'INACTIVE'
-WHERE latestpf.endDate < current_date();
+SET p.fundingStatus = 'CURRENT'
+WHERE latestpf.endDate >= current_date();
+
+UPDATE Post p
+INNER JOIN (SELECT f.postId, f.endDate FROM PostFunding f WHERE endDate IS NULL) latestpf ON p.id = latestpf.postId
+SET p.fundingStatus = 'CURRENT';
