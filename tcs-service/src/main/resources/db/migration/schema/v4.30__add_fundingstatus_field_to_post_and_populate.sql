a4acf234f2de8cd40a58719139bc7bb55ecf28da
Cai.Willis <cai.willis@hee.nhs.uk>
2024-03-18 10:27:43 +0000

    fix: correctly update null post with funding end date as null
    TIS21-5808: Add a new field fundingStatus in Post

--- tcs-service/src/main/resources/db/migration/schema/v4.30__add_fundingstatus_field_to_post_and_populate.sql
+++ tcs-service/src/main/resources/db/migration/schema/v4.30__add_fundingstatus_field_to_post_and_populate.sql
@@ -1,12 +1,13 @@
 ALTER TABLE `Post` ADD COLUMN `fundingStatus` varchar(255) NOT NULL;
 
-SET SQL_SAFE_UPDATES = 0;
 UPDATE Post p
 LEFT JOIN (SELECT f.postId, MAX(f.endDate) AS endDate FROM PostFunding f GROUP BY f.postId) latestpf ON p.id = latestpf.postId
 SET p.fundingStatus = 'CURRENT'
 WHERE latestpf.endDate >= current_date()
-OR latestpf.endDate IS NULL;
-SET SQL_SAFE_UPDATES = 1;
+
+UPDATE Post p
+INNER JOIN (SELECT f.postId, f.endDate AS endDate FROM PostFunding f WHERE endDate IS NULL) latestpf ON p.id = latestpf.postId
+SET p.fundingStatus = 'CURRENT';
 
 UPDATE Post p
 LEFT JOIN (SELECT f.postId, MAX(f.endDate) AS endDate FROM PostFunding f GROUP BY f.postId) latestpf ON p.id = latestpf.postId
