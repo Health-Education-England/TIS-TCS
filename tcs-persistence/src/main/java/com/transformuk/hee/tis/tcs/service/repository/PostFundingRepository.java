a1440cc8adc8dee777d035f49df597b179616963
Cai.Willis <cai.willis@hee.nhs.uk>
2024-04-04 11:35:00 +0100

    chore: code smell
    TIS21-5811: Update fundingStatus once a post funding is created/updated/deleted

--- tcs-persistence/src/main/java/com/transformuk/hee/tis/tcs/service/repository/PostFundingRepository.java
+++ tcs-persistence/src/main/java/com/transformuk/hee/tis/tcs/service/repository/PostFundingRepository.java
@@ -1,7 +1,6 @@
 package com.transformuk.hee.tis.tcs.service.repository;
 
 import com.transformuk.hee.tis.tcs.service.model.PostFunding;
-import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
