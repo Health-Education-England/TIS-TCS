217a19ca5581353b1d007c42d3a25c7ddaf50043
Cai.Willis <cai.willis@hee.nhs.uk>
2024-04-04 10:23:29 +0100

    fix: remove semicolon from spring query annotation
    TIS21-5811: Update fundingStatus once a post funding is created/updated/deleted

--- tcs-persistence/src/main/java/com/transformuk/hee/tis/tcs/service/repository/PostFundingRepository.java
+++ tcs-persistence/src/main/java/com/transformuk/hee/tis/tcs/service/repository/PostFundingRepository.java
@@ -16,7 +16,7 @@ public interface PostFundingRepository extends JpaRepository<PostFunding, Long> 
  @Query("SELECT count(id) FROM PostFunding\n"
      + "WHERE postId = :postId\n"
      + "AND startDate IS NOT NULL\n"
-     + "AND (endDate >= current_date() OR endDate IS NULL);")
+     + "AND (endDate >= current_date() OR endDate IS NULL)")
  long countCurrentFundings(@Param("postId") Long postId);
 
 }
