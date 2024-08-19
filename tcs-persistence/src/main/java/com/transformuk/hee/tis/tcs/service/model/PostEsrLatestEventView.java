740433b2f847585c114d40eb567e10ac30eb2b8d
Yafang Deng <hihilary@outlook.com>
2024-08-19 12:53:56 +0100

    fix:code smells

--- tcs-persistence/src/main/java/com/transformuk/hee/tis/tcs/service/model/PostEsrLatestEventView.java
+++ tcs-persistence/src/main/java/com/transformuk/hee/tis/tcs/service/model/PostEsrLatestEventView.java
@@ -1,14 +1,14 @@
 package com.transformuk.hee.tis.tcs.service.model;
 
 import com.transformuk.hee.tis.tcs.api.enumeration.PostEsrEventStatus;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.EnumType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.Immutable;
 
