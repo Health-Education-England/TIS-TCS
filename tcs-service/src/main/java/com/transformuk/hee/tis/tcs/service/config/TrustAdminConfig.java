3057d0600dcc5d3f4bba5cf6f7892d6c4e47396e
Andy Dingley <Judge40@users.noreply.github.com>
2024-09-16 14:58:01 +0100

    fix: use machine user to call reference
    The GMC Validator checks the given GMC status against the reference
    service to check if the value is valid. However, calls to the erference
    service use the current user's authentication token. As the new GMC
    update is received via RabbitMQ there is no authenticated user to
    provide a token.
    
    Update ReferenceClientProdConfig to use a machine user token instead of
    requiring an actual user token.
    
    TIS21-6292
    TIS21-6479

--- tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/TrustAdminConfig.java
+++ tcs-service/src/main/java/com/transformuk/hee/tis/tcs/service/config/TrustAdminConfig.java
@@ -3,8 +3,6 @@ package com.transformuk.hee.tis.tcs.service.config;
 import com.transformuk.hee.tis.security.client.KeycloakClientRequestFactory;
 import com.transformuk.hee.tis.security.client.KeycloakRestTemplate;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.KeycloakBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -17,45 +15,18 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class TrustAdminConfig {
 
-  @Value("${kc.realm}")
-  private String realm;
-
-  @Value("${kc.client.id}")
-  private String clientId;
-
-  @Value("${kc.server.url}")
-  private String serverUrl;
-
-  @Value("${kc.username}")
-  private String userName;
-
-  @Value("${kc.password}")
-  private String password;
-
-
   /**
    * Rest template used to communicate with other services. Talks to keycloak to get a OIDC token
    * before making the request. Logs into KC using the TCS user credentials
    *
-   * @return
+   * @param keycloak The keycloak client to use.
+   * @return The rest template.
    */
   @Bean
-  public RestTemplate trustAdminEnabledRestTemplate() {
-    final KeycloakClientRequestFactory keycloakClientRequestFactory = new KeycloakClientRequestFactory(
-        getKc());
+  public RestTemplate trustAdminEnabledRestTemplate(Keycloak keycloak) {
+    final KeycloakClientRequestFactory keycloakClientRequestFactory =
+        new KeycloakClientRequestFactory(keycloak);
     return new KeycloakRestTemplate(keycloakClientRequestFactory);
 
   }
-
-  private Keycloak getKc() {
-    Keycloak kc = KeycloakBuilder.builder()
-        .serverUrl(serverUrl)
-        .realm(realm)
-        .clientId(clientId)
-        .username(userName)
-        .password(password)
-        .build();
-    return kc;
-  }
-
 }
