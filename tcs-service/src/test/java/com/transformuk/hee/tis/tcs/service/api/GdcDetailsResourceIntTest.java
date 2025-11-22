298b5cca09d194fd8a9647530eb07bd1cb218f89
Reuben Roberts <reuben.roberts@nhs.net>
2025-11-22 09:41:47 +0000

    fix: remove unused import

--- tcs-service/src/test/java/com/transformuk/hee/tis/tcs/service/api/GdcDetailsResourceIntTest.java
+++ tcs-service/src/test/java/com/transformuk/hee/tis/tcs/service/api/GdcDetailsResourceIntTest.java
@@ -39,7 +39,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
