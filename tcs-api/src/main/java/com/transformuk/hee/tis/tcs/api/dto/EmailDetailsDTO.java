61755d62085b63ce58656d308293e65ee08adf0d
Reuben Roberts <reuben.roberts@nhs.net>
2026-01-23 09:23:14 +0000

    fix: simplify DTO since equals() and hashcode() defaults ok

--- tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/dto/EmailDetailsDTO.java
+++ tcs-api/src/main/java/com/transformuk/hee/tis/tcs/api/dto/EmailDetailsDTO.java
@@ -4,7 +4,6 @@ import com.transformuk.hee.tis.tcs.api.dto.validation.Create;
 import com.transformuk.hee.tis.tcs.api.dto.validation.TraineeUpdate;
 import com.transformuk.hee.tis.tcs.api.dto.validation.Update;
 import java.io.Serializable;
-import java.util.Objects;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.Data;
@@ -20,25 +19,4 @@ public class EmailDetailsDTO implements Serializable {
   @Email(message = "Valid email format required",
       groups = {Update.class, Create.class, TraineeUpdate.class})
   private String email;
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-
-    EmailDetailsDTO emailDetailsDTO = (EmailDetailsDTO) o;
-    if (emailDetailsDTO.getEmail() == null || getEmail() == null) {
-      return false;
-    }
-    return Objects.equals(getEmail(), emailDetailsDTO.getEmail());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(getEmail());
-  }
 }
