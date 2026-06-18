1b12a777f51e62e71fa4646327bdc7a7ea66ed1c
jayanta2018 <jayanta.saha@hee.nhs.uk>
2026-06-18 15:37:51 +0100

    feat: add new sql

--- tcs-persistence/src/main/resources/queries/postViewForES.sql
+++ tcs-persistence/src/main/resources/queries/postViewForES.sql
@@ -1,4 +1,4 @@
-select distinct id,
+select id,
 approvedGradeId,
 primarySpecialtyId,
 primarySpecialtyCode,
@@ -9,38 +9,47 @@ GROUP_CONCAT(distinct fundingType SEPARATOR ', ') fundingType,
 nationalPostNumber,
 fundingStatus,
 owner,
-GROUP_CONCAT(surnames SEPARATOR ', ') surnames,
-GROUP_CONCAT(forenames SEPARATOR ', ') forenames,
+surnames,
+forenames,
 GROUP_CONCAT(distinct trustId SEPARATOR ',') trustIds,
 GROUP_CONCAT(distinct programmeId SEPARATOR ',') programmeIds
- from (SELECT p.`id`,
-    pg.`gradeId` as `approvedGradeId`,
-    ps.`specialtyId` as `primarySpecialtyId`,
-    sp.`specialtyCode` as `primarySpecialtyCode`,
-    sp.`name` as `primarySpecialtyName`,
-    pst.`siteId` as `primarySiteId`,
-    prg.`programmeName`,
-    pf.`fundingType`,
-    p.`nationalPostNumber`,
-    p.`fundingStatus`,
-    p.`owner`,
-    c.surname surnames, c.forenames forenames,
-    pt.`trustId` as `trustId`,
-    pp.`programmeId` as `programmeId`
+from (SELECT p.`id`,
+        pg.`gradeId` as `approvedGradeId`,
+        ps.`specialtyId` as `primarySpecialtyId`,
+        sp.`specialtyCode` as `primarySpecialtyCode`,
+        sp.`name` as `primarySpecialtyName`,
+        pst.`siteId` as `primarySiteId`,
+        prg.`programmeName`,
+        pf.`fundingType`,
+        p.`nationalPostNumber`,
+        p.`fundingStatus`,
+        p.`owner`,
+        trainees.`surnames`,
+        trainees.`forenames`,
+        pt.`trustId` as `trustId`,
+        pp.`programmeId` as `programmeId`
     FROM `Post` p
     LEFT JOIN `PostGrade` pg on p.`id` = pg.`postId` AND pg.`postGradeType` = 'APPROVED'
     LEFT JOIN `PostSpecialty` ps on p.`id` = ps.`postId` AND ps.`postSpecialtyType` = 'PRIMARY'
     LEFT JOIN `Specialty` sp on sp.`id` = ps.`specialtyId`
     LEFT JOIN `PostSite` pst on p.`id` = pst.`postId` AND pst.`postSiteType` = 'PRIMARY'
     LEFT JOIN `PostFunding` pf on p.`id` = pf.`postId` and (curdate() BETWEEN pf.startDate AND pf.endDate or pf.endDate is NULL)
-    LEFT JOIN `Placement` pl on pl.postId = p.id and curdate() BETWEEN pl.dateFrom AND pl.dateTo
-    LEFT JOIN `ContactDetails` c on pl.traineeId = c.id
-    LEFT JOIN `ProgrammePost` pp on pp.postId = p.id
+    LEFT JOIN (
+        SELECT pl.`postId`,
+		    GROUP_CONCAT(DISTINCT c.`surname` SEPARATOR ', ') AS `surnames`,
+        GROUP_CONCAT(DISTINCT c.`forenames` SEPARATOR ', ') AS `forenames`
+        FROM `Placement` pl
+        LEFT JOIN `ContactDetails` c on pl.`traineeId` = c.`id`
+		WHERE curdate() BETWEEN pl.`dateFrom` AND pl.`dateTo`
+        GROUP BY pl.`postId`
+    ) trainees on trainees.`postId` = p.`id`
+    LEFT JOIN `ProgrammePost` pp on pp.`postId` = p.`id`
     LEFT JOIN `Programme` prg on prg.`id` = pp.`programmeId`
     LEFT JOIN `PostTrust` pt on pt.`postId` = p.`id`
- WHERECLAUSE
+WHERECLAUSE
 ) as ot
-group by id,approvedGradeId,primarySpecialtyId,primarySpecialtyCode,primarySpecialtyName,primarySiteId
- ORDERBYCLAUSE
- LIMITCLAUSE
+group by id,approvedGradeId,primarySpecialtyId,primarySpecialtyCode,primarySpecialtyName,
+primarySiteId,nationalPostNumber,fundingStatus,owner,surnames,forenames
+ORDERBYCLAUSE
+LIMITCLAUSE
 ;
