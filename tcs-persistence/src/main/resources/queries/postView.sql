6f5feda2576d64498325a32e829d4692f5cdc23c
NM2.7JBY9KP8 <cai.willis@nhs.net>
2026-06-23 11:22:20 +0100

    fix: prevent exclusion of genuine duplicate names

--- tcs-persistence/src/main/resources/queries/postView.sql
+++ tcs-persistence/src/main/resources/queries/postView.sql
@@ -33,12 +33,16 @@ from (SELECT p.`id`,
     LEFT JOIN `PostSite` pst on p.`id` = pst.`postId` AND pst.`postSiteType` = 'PRIMARY'
     LEFT JOIN `PostFunding` pf on p.`id` = pf.`postId` and (curdate() BETWEEN pf.startDate AND pf.endDate or pf.endDate is NULL)
     LEFT JOIN (
-        SELECT pl.`postId`,
-        GROUP_CONCAT(DISTINCT c.`surname` SEPARATOR ', ') AS `surnames`,
-        GROUP_CONCAT(DISTINCT c.`forenames` SEPARATOR ', ') AS `forenames`
-        FROM `Placement` pl
-        LEFT JOIN `ContactDetails` c on pl.`traineeId` = c.`id` WHERE curdate() BETWEEN pl.`dateFrom` AND pl.`dateTo`
-        GROUP BY pl.`postId`
+      SELECT t.`postId`,
+        GROUP_CONCAT(t.`surname` ORDER BY t.`traineeId` SEPARATOR ', ') AS `surnames`,
+        GROUP_CONCAT(t.`forenames` ORDER BY t.`traineeId` SEPARATOR ', ') AS `forenames`
+        FROM (
+          SELECT DISTINCT pl.`postId`, pl.`traineeId`, c.`surname`, c.`forenames`
+          FROM `Placement` pl
+          LEFT JOIN `ContactDetails` c on pl.`traineeId` = c.`id`
+          WHERE curdate() BETWEEN pl.`dateFrom` AND pl.`dateTo`
+         ) t
+     GROUP BY t.`postId`
     ) trainees on trainees.`postId` = p.`id`
     LEFT JOIN `ProgrammePost` pp on pp.`postId` = p.`id`
     LEFT JOIN `Programme` prg on prg.`id` = pp.`programmeId`
