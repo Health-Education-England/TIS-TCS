636ae1db928dc89d43d25a84740c287c050886a3
jayanta2018 <jayanta.saha@hee.nhs.uk>
2026-06-23 12:09:19 +0100

    fix: handle the review comments

--- tcs-persistence/src/main/resources/queries/postView.sql
+++ tcs-persistence/src/main/resources/queries/postView.sql
@@ -1,4 +1,4 @@
-select id,
+select distinct id,
 approvedGradeId,
 primarySpecialtyId,
 primarySpecialtyCode,
@@ -33,20 +33,23 @@ from (SELECT p.`id`,
     LEFT JOIN `PostSite` pst on p.`id` = pst.`postId` AND pst.`postSiteType` = 'PRIMARY'
     LEFT JOIN `PostFunding` pf on p.`id` = pf.`postId` and (curdate() BETWEEN pf.startDate AND pf.endDate or pf.endDate is NULL)
     LEFT JOIN (
-        SELECT pl.`postId`,
-        GROUP_CONCAT(DISTINCT c.`surname` SEPARATOR ', ') AS `surnames`,
-        GROUP_CONCAT(DISTINCT c.`forenames` SEPARATOR ', ') AS `forenames`
+      SELECT t.`postId`,
+      GROUP_CONCAT(t.`surname` ORDER BY t.`traineeId` SEPARATOR ', ') AS `surnames`,
+      GROUP_CONCAT(t.`forenames` ORDER BY t.`traineeId` SEPARATOR ', ') AS `forenames`
+      FROM (
+        SELECT DISTINCT pl.`postId`, pl.`traineeId`, c.`surname`, c.`forenames`
         FROM `Placement` pl
-        LEFT JOIN `ContactDetails` c on pl.`traineeId` = c.`id` WHERE curdate() BETWEEN pl.`dateFrom` AND pl.`dateTo`
-        GROUP BY pl.`postId`
+        LEFT JOIN `ContactDetails` c on pl.`traineeId` = c.`id`
+        WHERE curdate() BETWEEN pl.`dateFrom` AND pl.`dateTo`
+       ) t
+       GROUP BY t.`postId`
     ) trainees on trainees.`postId` = p.`id`
     LEFT JOIN `ProgrammePost` pp on pp.`postId` = p.`id`
     LEFT JOIN `Programme` prg on prg.`id` = pp.`programmeId`
-    TRUST_JOIN
+TRUST_JOIN
 WHERECLAUSE
 ) as ot
-group by id,approvedGradeId,primarySpecialtyId,primarySpecialtyCode,primarySpecialtyName,
-primarySiteId,nationalPostNumber,fundingStatus,owner,surnames,forenames,intrepidId
+group by id,approvedGradeId,primarySpecialtyId,primarySpecialtyCode,primarySpecialtyName,primarySiteId,surnames,forenames
 ORDERBYCLAUSE
 LIMITCLAUSE
 ;
